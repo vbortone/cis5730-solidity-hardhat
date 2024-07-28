49c11bfe566459aafd43eeef03f7faebebd4506e
Vincent Bortone <vbortone@gmail.com>
2024-07-28 01:53:38 -0400

    feat: add stubs for Lending Platform

---
+++ contracts/LendingPlatform.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract LendingPlatform {
+    constructor(IERC20 _token, uint256 _interestRate) {}
+    function lend(uint256 _amount) public{}
+    function borrow(uint256 _amount) public{}
+    function repay() public{}
+    function calculateInterest(uint256 _amount, uint256 _duration) internal view returns (uint256){}
+}
